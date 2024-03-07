070e60aef2cc22c6fea096d8f13779596b7e4b42
mishgan2000 <yunoshev.mikhail@gmail.com>
2024-03-07 09:22:14 +0700

    Создал новый процессор для рабочего проекта (в корне проекта, папка system).

--- proc/proc.exportToSDK.tcl
+++ proc/proc.exportToSDK.tcl
@@ -13,4 +13,14 @@ if { [catch {exportToSDK} result] } {
   exit -1
 }
 
+set sExportDir [ xget sdk_export_dir ]
+set sExportDir [ file join "C:/Work/XILINX/Projects/New_25/proc" "$sExportDir" "hw" ] 
+if { [ file exists C:/Work/XILINX/Projects/New_25/edkBmmFile_bd.bmm ] } {
+   puts "Copying placed bmm file C:/Work/XILINX/Projects/New_25/edkBmmFile_bd.bmm to $sExportDir" 
+   file copy -force "C:/Work/XILINX/Projects/New_25/edkBmmFile_bd.bmm" $sExportDir
+}
+if { [ file exists C:/Work/XILINX/Projects/New_25/top.bit ] } {
+   puts "Copying bit file C:/Work/XILINX/Projects/New_25/top.bit to $sExportDir" 
+   file copy -force "C:/Work/XILINX/Projects/New_25/top.bit" $sExportDir
+}
 exit $result
---
+++ system/mcb_system/Debug/src/platform.d
@@ -0,0 +1,34 @@
+src/platform.d: ../src/platform.c \
+ ../../mcb_system_bsp/microblaze_0/include/xparameters.h \
+ ../../mcb_system_bsp/microblaze_0/include/xil_cache.h \
+ ../../mcb_system_bsp/microblaze_0/include/mb_interface.h \
+ ../../mcb_system_bsp/microblaze_0/include/xbasic_types.h \
+ ../../mcb_system_bsp/microblaze_0/include/xil_types.h \
+ ../../mcb_system_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h \
+ ../../mcb_system_bsp/microblaze_0/include/xuartns550_l.h \
+ ../../mcb_system_bsp/microblaze_0/include/xil_assert.h \
+ ../../mcb_system_bsp/microblaze_0/include/xil_io.h \
+ ../../mcb_system_bsp/microblaze_0/include/xil_printf.h
+
+../../mcb_system_bsp/microblaze_0/include/xparameters.h:
+
+../../mcb_system_bsp/microblaze_0/include/xil_cache.h:
+
+../../mcb_system_bsp/microblaze_0/include/mb_interface.h:
+
+../../mcb_system_bsp/microblaze_0/include/xbasic_types.h:
+
+../../mcb_system_bsp/microblaze_0/include/xil_types.h:
+
+../../mcb_system_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
+
+../../mcb_system_bsp/microblaze_0/include/xuartns550_l.h:
+
+../../mcb_system_bsp/microblaze_0/include/xil_assert.h:
+
+../../mcb_system_bsp/microblaze_0/include/xil_io.h:
+
+../../mcb_system_bsp/microblaze_0/include/xil_printf.h:
